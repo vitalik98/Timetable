e0e98963372eea565fcac11f3edf76b227bb25ad
Vitali Mazanik <vitalii.mazanik.98@gmail.com>
2018-05-10 12:30:29 +0300

    Update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,22 +1,23 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { JsonProvider} from "../../providers/json/json";
-import { TimetablePage } from "../timetable/timetable";
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {JsonProvider} from "../../providers/json/json";
+import {TimetablePage} from "../timetable/timetable";
 
 export class Teacher {
-  name:string;
-  days:Day;
+  name: string;
+  days: Day;
 }
+
 export class Day {
-  dayN:string;
-  subjects:Subject;
+  dayN: string;
+  subjects: Subject;
 }
 
 export class Subject {
-  time:string;
-  subject:string;
-  course:number;
-  group:number;
+  time: string;
+  subject: string;
+  course: number;
+  group: number;
 }
 
 @Component({
@@ -24,16 +25,31 @@ export class Subject {
   templateUrl: 'home.html'
 })
 export class HomePage {
-  teachers:Teacher[];
+  teachers: Teacher[];
   days;
+  teacherBuf:Teacher[];
   constructor(public navCtrl: NavController, public JsonP: JsonProvider) {
+  }
 
+  showTimetable(days) {
+    this.navCtrl.push(TimetablePage, {days: days})
   }
-  showTimetable(days){
-    this.navCtrl.push(TimetablePage,{days:days})
+
+  ionViewDidLoad() {
+    this.JsonP.getData().subscribe(data => this.teachers = data['teachers']);
+    this.JsonP.getData().subscribe(data => this.teacherBuf = data['teachers']);
+    this.JsonP.getData().subscribe(data => console.log("!!!", data));
   }
-  ionViewDidLoad(){
-    this.JsonP.getData().subscribe(data=>this.teachers=data['teachers']);
-    this.JsonP.getData().subscribe(data=>console.log("!!!",data));
+  getItems(ev: any) {
+    let val = ev.target.value;
+    console.log(val);
+    if (val == '' || val == null || val == undefined) {
+      this.teacherBuf = this.teachers;
+    }
+    else {
+      this.teacherBuf = this.teachers.filter((teacher) => {
+        return (teacher.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 }
